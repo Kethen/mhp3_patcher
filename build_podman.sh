242d45d2333028edf4e77172dc76e7997a3a3314
Katharine Chui <katharine.chui@gmail.com>
2026-02-01 20:58:45 +0100

    add github action and local build

---
+++ build_podman.sh
@@ -0,0 +1,15 @@
+TAG="mhp3_patcher_pspsdk"
+
+if ! podman image exists $TAG
+then
+	podman image build -t $TAG -f Dockerfile
+fi
+
+podman run \
+	--rm -it \
+	-v ./:/work_dir \
+	-w /work_dir \
+	$TAG \
+	-c "
+make
+"
